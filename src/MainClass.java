6829f4ebeecfe6b262a9cb3e59e5c7b429fe091c
Giuseppe Antonio Sansone <giuseppeantonio.sansone@gmail.com>
2017-10-28 17:46:47 +0200

    Complite read/write process

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,8 +3,19 @@ import java.util.Iterator;
 
 import database.DatabaseConnectionException;
 import database.DbEngine;
+import interfaces.WPPostInterface;
+import interfaces.WPPostMetaInterface;
 import interfaces.WPUserInterface;
+import interfaces.WPUserMetaInterface;
+import interfaces.WriteWPPostException;
+import interfaces.WriteWPPostMetaException;
+import interfaces.WriteWPUserException;
+import interfaces.WriteWPUserMetaException;
+import keyboard.Keyboard;
+import model.WPPost;
+import model.WPPostMeta;
 import model.WPUser;
+import model.WPUserMeta;
 
 /**
  * @author giuseppe
@@ -107,6 +118,7 @@ public class MainClass
 			srcPass = "180993";
 			destUser = "root";
 			destPass = "180993";
+			
 			DbEngine srcDbEngine = new DbEngine();
 			try 
 			{
@@ -131,22 +143,241 @@ public class MainClass
 				System.exit(1);
 			}
 			
+			// Reading from wp_users
+			System.out.println("Reading records from table wp_users...");
 			WPUserInterface srcUserInterface = new WPUserInterface(new WPUser(), srcDbEngine.getConnection());
 			try 
 			{
 				srcUserInterface.readTable();
-				Iterator<WPUser> itr = srcUserInterface.getReadValues().iterator();
 				
-				while(itr.hasNext())
+				if(srcUserInterface.getReadValues().isEmpty())
 				{
-					WPUser usr = itr.next();
-					System.out.println(usr.toString());
+					System.out.println("Pay Attention!: No records have been read from the table wp_users.");
+				}
+				else
+				{
+					System.out.println("Read " + srcUserInterface.getReadValues().size()
+							+ "records from table wp_users.");
+					
+					System.out.println("Do you want to list the records? (y/n)");
+					String choose = Keyboard.readString();
+					
+					Iterator<WPUser> itr = srcUserInterface.getReadValues().iterator();
+					
+					if(choose.equalsIgnoreCase("y"))
+					{
+						int i = 1;
+						while(itr.hasNext())
+						{
+							WPUser usr = itr.next();
+							System.out.println(i + "--> " + usr.toString());
+							i++;
+						}
+					}
+					
+					//Writing wp_users
+					System.out.println("Writing to wp_users...");
+					WPUserInterface dstUserInterface = new WPUserInterface(new WPUser(), destDbEngine.getConnection());
+					
+					dstUserInterface.setReadValues(srcUserInterface.getReadValues());
+					try 
+					{
+						dstUserInterface.writeTable();
+					} 
+					catch (WriteWPUserException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination users table.");
+					} 
+					catch (SQLException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination users table.");
+					}
 				}
 			} 
 			catch (SQLException e) 
 			{
 				e.printStackTrace();
 				System.out.println("Error: Failed to read the origin users table.");
+			}
+			
+			
+			
+			//Reading wp_usermeta
+			System.out.println("Reading records from table wp_usermeta...");
+			WPUserMetaInterface wpUserMeta = new WPUserMetaInterface(new WPUserMeta(), srcDbEngine.getConnection());
+			try 
+			{
+				wpUserMeta.readTable();
+				
+				if(wpUserMeta.getReadValues().isEmpty())
+				{
+					System.out.println("Pay Attention!: No records have been read from the table wp_usermeta.");
+				}
+				else
+				{
+					System.out.println("Read " + wpUserMeta.getReadValues().size()
+							+ "records from table wp_usermeta.");
+					
+					System.out.println("Do you want to list the records? (y/n)");
+					String choose = Keyboard.readString();
+					
+					Iterator<WPUserMeta> itr = wpUserMeta.getReadValues().iterator();
+					
+					if(choose.equalsIgnoreCase("y"))
+					{
+						int i = 1;
+						while(itr.hasNext())
+						{
+							WPUserMeta usrMeta = itr.next();
+							System.out.println(i + "--> " + usrMeta.toString());
+							i++;
+						}
+					}
+					
+					//Writing wp_users
+					System.out.println("Writing to wp_usermeta...");
+					WPUserMetaInterface dstUserMetaInterface = 
+							new WPUserMetaInterface(new WPUserMeta(), destDbEngine.getConnection());
+					
+					dstUserMetaInterface.setReadValues(wpUserMeta.getReadValues());
+					try 
+					{
+						dstUserMetaInterface.writeTable();
+					} 
+					catch (WriteWPUserMetaException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination wp_usermeta table.");
+					} 
+					catch (SQLException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination wp_usermeta table.");
+					}
+				}
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+				System.out.println("Error: Failed to read the origin wp_usermeta table.");
+			}
+			
+			//Reading wp_posts
+			System.out.println("Reading records from table wp_posts...");
+			WPPostInterface wpPost = new WPPostInterface(new WPPost(), srcDbEngine.getConnection());
+			
+			try 
+			{
+				wpPost.readTable();
+				
+				if(wpPost.getReadValues().isEmpty())
+				{
+					System.out.println("Pay Attention!: No records have been read from the table wp_posts.");
+				}
+				else
+				{
+					System.out.println("Read " + wpPost.getReadValues().size()
+							+ "records from table wp_posts.");
+					
+					System.out.println("Do you want to list the records? (y/n)");
+					String choose = Keyboard.readString();
+					
+					Iterator<WPPost> itr = wpPost.getReadValues().iterator();
+					
+					if(choose.equalsIgnoreCase("y"))
+					{
+						int i = 1;
+						while(itr.hasNext())
+						{
+							WPPost post = itr.next();
+							System.out.println(i + "--> " + post.toString());
+							i++;
+						}
+					}
+					
+					//Writing wp_posts
+					System.out.println("Writing to wp_posts...");
+					WPPostInterface dstPostInterface = new WPPostInterface(new WPPost(), destDbEngine.getConnection());
+					
+					dstPostInterface.setReadValues(wpPost.getReadValues());
+					try 
+					{
+						dstPostInterface.writeTable();
+					}
+					catch (SQLException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination wp_posts table.");
+					}
+					
+				}
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+				System.out.println("Error: Failed to read the origin wp_posts table.");
+			}
+			
+			//Reading wp_postmeta
+			System.out.println("Reading records from table wp_postmeta...");
+			WPPostMetaInterface wpPostMeta = new WPPostMetaInterface(new WPPostMeta(), srcDbEngine.getConnection());
+			
+			try 
+			{
+				wpPostMeta.readTable();
+
+				if(wpPostMeta.getReadValues().isEmpty())
+				{
+					System.out.println("Pay Attention!: No records have been read from the table wp_postmeta.");
+				}
+				else
+				{
+					System.out.println("Read " + wpPostMeta.getReadValues().size()
+							+ "records from table wp_postmeta.");
+					
+					System.out.println("Do you want to list the records? (y/n)");
+					String choose = Keyboard.readString();
+					
+					Iterator<WPPostMeta> itr = wpPostMeta.getReadValues().iterator();
+					
+					if(choose.equalsIgnoreCase("y"))
+					{
+						int i = 1;
+						while(itr.hasNext())
+						{
+							WPPostMeta postMeta = itr.next();
+							System.out.println(i + "--> " + postMeta.toString());
+							i++;
+						}
+					}
+					
+					//Writing wp_postmeta
+					System.out.println("Writing to wp_postmeta...");
+					WPPostMetaInterface dstPostMetaInterface = 
+							new WPPostMetaInterface(new WPPostMeta(), destDbEngine.getConnection());
+					
+					dstPostMetaInterface.setReadValues(wpPostMeta.getReadValues());
+					try 
+					{
+						dstPostMetaInterface.writeTable();
+					}
+					catch (SQLException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination wp_postmeta table.");
+					} catch (WriteWPPostMetaException e) 
+					{
+						e.printStackTrace();
+						System.out.println("Error: Failed to write the destination wp_postmeta table.");
+					}
+				}
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+				System.out.println("Error: Failed to read the origin wp_postmeta table.");
 			}
 		}
 	}
